b601882097fa781d2ecea4f86772fb61bd3afbdf
Dan Hirst <danhirst123@gmail.com>
2020-04-16 14:58:38 +0100

    UI FIX clear the field when adding a new group

--- FlashCards/FlashCards/Page0/FirstPageViewModel.cs
+++ FlashCards/FlashCards/Page0/FirstPageViewModel.cs
@@ -179,6 +179,7 @@ namespace FlashCards.Page0
             Groups.Cards = FlashCards;
             Groups.Save();
             getListOfGroups(FlashCards);
+            NewGroup = "";
         }
 
 
